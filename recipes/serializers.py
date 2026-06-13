187d7bac4d195e57fe239b66833433ffc41753a8
Anjita <156284237+anjitakandel41@users.noreply.github.com>
2026-06-13 22:10:18 +0545

    Refactor serializers to specify fields explicitly

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -4,12 +4,40 @@ from .models import Category, Recipe
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
+
 
 class RecipeSerializer(serializers.ModelSerializer):
-    author_username = serializers.ReadOnlyField(source="author.username")
+    author_username = serializers.ReadOnlyField(
+        source="author.username"
+    )
 
     class Meta:
         model = Recipe
-        fields = "__all__"
-        read_only_fields = ["author","created_at","updated_at"]
+        fields = [
+            "id",
+            "title",
+            "description",
+            "ingredients",
+            "steps",
+            "cooking_time",
+            "photo",
+            "category",
+            "author",
+            "author_username",
+            "created_at",
+            "updated_at",
+        ]
+
+        read_only_fields = [
+            "author",
+            "author_username",
+            "created_at",
+            "updated_at",
+        ]
